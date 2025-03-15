d8e97b1f3b16cce720f4f56b8e306b0ba39840cc
Elias <elxssnone@gmail.com>
2025-03-15 23:02:18 +0100

    Cooking

--- MegaUp.net AUTO-DL.js
+++ MegaUp.net AUTO-DL.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name         MegaUp.net AUTO-DL
-// @version      0.1
+// @name         MegaUp.net Auto Downloader
+// @version      1.1
 // @description  Auto DL script for MegaUp.net
 // @author       Elxss
 // @match        *://*.megaup.net/*
@@ -10,19 +10,56 @@
 // @run-at       document-start
 // ==/UserScript==
 
-console.log('[AUTO-DL] DL Ready');
+// Last Updated and Tested the 15/03/2025
+// I Hope you enjoy it :)
 
-setTimeout(function() {
-    var selection = document.getElementById("btnsubmit") !== null;
-    if (selection) {
-        console.log('[AUTO-DL] DL available');
-        document.getElementById("btnsubmit").click();
-        document.getElementById("btnsubmit").click();
-        document.getElementById("btnsubmit").click();
-    } else {
-        console.log('[AUTO-DL] DL unavailable');
-        'use strict';
-        window.close();
-    }
-}, 15000);
+console.log("[AUTO-DL] Loaded ! Version 1.1 If it doesn't work anymore let me know :)");
 
+(function() {
+    'use strict';
+
+    let Confirmed = false;
+
+    const checkButtonGenerateLink = setInterval(() => {
+        let TimerDiv = document.querySelector("div[class='download-timer']");
+
+        console.log('[AUTO-DL] [ Stage 1 ] Checking ...');
+        if (TimerDiv) {
+            if (Confirmed===false) {
+                Confirmed = true;
+                console.log('[AUTO-DL] [ Stage 1 ] Confirmed Stoping Stage 2 Check');
+            }
+            clearInterval(checkButtonDownload);
+            let AExist = TimerDiv.querySelector("a");
+            if (AExist) {
+                console.log("[AUTO-DL] [ Stage 1 ] HIT !");
+                AExist.click();
+                clearInterval(checkButtonGenerateLink);
+            }
+        }
+    }, 1000);
+
+    const checkButtonDownload = setInterval(() => {
+        let DownloadDiv = document.querySelector("div[class='download']");
+
+        console.log('[AUTO-DL] [ Stage 2 ] Checking ...');
+        if (DownloadDiv) {
+            if (Confirmed===false) {
+                Confirmed = true;
+                console.log('[AUTO-DL] [ Stage 2 ] Confirmed Stoping Stage 1 Check');
+                clearInterval(checkButtonGenerateLink);
+            }
+
+            let ButtonExist = DownloadDiv.querySelector("button[id='btndownload']");
+
+            if (ButtonExist && !ButtonExist.classList.contains("disable")) {
+                console.log("[AUTO-DL] [ Stage 2 ] Jackpot :)");
+                ButtonExist.click();
+                clearInterval(checkButtonDownload);
+                setTimeout(() => window.close(), 3000);
+            }
+        }
+    }, 1000);
+
+
+})();
